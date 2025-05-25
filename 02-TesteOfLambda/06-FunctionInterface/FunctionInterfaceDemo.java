c1d25329627e8fdbb387534c42e61d209f4e6d66
cloudshare360 <cloud.share360@gmail.com>
2025-05-25 20:11:53 +0000

    updated
    Signed-off-by: cloudshare360 <cloud.share360@gmail.com>

--- 02-TesteOfLambda/06-FunctionInterface/FunctionInterfaceDemo.java
+++ 02-TesteOfLambda/06-FunctionInterface/FunctionInterfaceDemo.java
@@ -1,15 +1,22 @@
 import java.util.function.Function;
- 
+
 public class FunctionInterfaceDemo {
- 
+
     public static void main(String[] args) {
         // Created a function which returns the length of string.
         Function<String, Integer> lengthFunction = str -> str.length();
-        
+
         System.out.println("String length: " + lengthFunction.apply("This is awesome!!"));
 
         Function<String, String> toUpperCase = s -> s.toUpperCase();
-System.out.println("Uppercase: " + toUpperCase.apply("hello")); 
- 
+        System.out.println("Uppercase: " + toUpperCase.apply("hello"));
+
+        String expected = "hello";
+
+        Function<String, Boolean> isInputEqualToExpected = s -> expected.equals(s);
+
+        System.out.println("Result: " + isInputEqualToExpected.apply("hello")); // true
+        System.out.println("Result: " + isInputEqualToExpected.apply("hi")); // false
+
     }
 }
