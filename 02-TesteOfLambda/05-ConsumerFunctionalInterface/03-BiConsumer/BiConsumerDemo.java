fd94506a05d851b9658581200fdff418f48831d1
srinivasaraogurram <srinivasarao.gurram@gmail.com>
2025-05-25 14:37:59 +0000

    updated BiConsumerDemo

--- 02-TesteOfLambda/05-ConsumerFunctionalInterface/03-BiConsumer/BiConsumerDemo.java
+++ 02-TesteOfLambda/05-ConsumerFunctionalInterface/03-BiConsumer/BiConsumerDemo.java
@@ -4,7 +4,7 @@ public class BiConsumerDemo {
 
     public static void main(String[] args) {
 
-      BiConsumer<String, String> greet = (s1, s2) -> System.out.println(s1 + s2);
+      BiConsumer<String, String> greet = (s1, s2) -> System.out.println(s1 + " "+ s2);
       greet.accept("Hello", "World");
 
     }
