792882340e8b284aa44c2eda655b8998d8f74240
cloudshare360 <cloud.share360@gmail.com>
2025-05-27 20:51:44 +0000

    updated configuration
    Signed-off-by: cloudshare360 <cloud.share360@gmail.com>

---
+++ 03-Stream-API/03-Mapping-Operations-in-Stream/StreamDemoMap.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamDemoMap {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Dave");
+        list.add("Joe");
+        list.add("Ryan");
+        list.add("Iyan");
+        list.add("Ray");
+        // map() is used to convert each name to upper case.
+        // Note: The map() method does not modify the original list.
+        list.stream()
+                .map(name -> name.toUpperCase()) //map() takes an input of Function<T, R> type.
+                .forEach(System.out::println);   // forEach() takes an input of Consumer type.
+        //sorted
+        List<String> sortedUpperList = list.stream()
+    .sorted()
+    .map(String::toUpperCase)
+    .collect(Collectors.toList());
+
+System.out.println(sortedUpperList);
+       
+
+    }
+}
