247ba804b7821d02809b5989f1034d8a09d1435a
CloudShare360 <cloud.share360@gmail.com>
2025-06-07 21:56:21 +0000

    adding optional interfaces project

--- 03-Stream-API/04-Method-References/MethodReferenceApp/src/App.java
+++ 03-Stream-API/04-Method-References/MethodReferenceApp/src/App.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        // Method References
+        Consumer<String> consumer = s -> System.out.println(s);
+        Consumer<String> consumerWithMethodRef = System.out::println;
+
+        consumer.accept("Using Lambda");
+        consumerWithMethodRef.accept("Using Method Reference");
+
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
+
+        names.forEach(consumer); // or consumerWithMethodRef
+
+        names.forEach(consumerWithMethodRef); // or consumerWithMethodRef
+
     }
 }
